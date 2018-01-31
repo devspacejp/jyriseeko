a3bf65ebe5e7254be0b0b6ca2ba4cdd0e683f07d
devspacejp <jaakko.pullinen@gmail.com>
2018-01-31 16:03:54 +0200

    Delete jyri_sala.py

--- jyri_sala.py
+++
@@ -1,7 +0,0 @@
-# Jyrinäbotin conffi filu.
-# Twitter API access authit
-consumer_key = 	'Bir7wkC2C1xq20dLEjWqiAqNR'
-consumer_secret = 'Bkfmo67REUIw9V9cCCXmlYW8NXYt6LQ3foF4YsdxnOaU2z8dA2'
-access_token = '878179836430356480-P6DvYVPjUkCazIH1SIVBvMzAKeJCgu7'
-access_secret = 'TOATtIOreRbvd2Hxym4NwAnNbXxBS8zMuIuCKwBCasSP3'
-
